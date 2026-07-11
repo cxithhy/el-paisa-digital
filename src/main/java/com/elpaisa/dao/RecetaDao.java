ca332e067fae5d7cbcb6b1b1c164295ee92a63e2
Equipo El Paisa Digital <equipo@elpaisadigital.com>
2026-07-10 20:40:57 -0500

    chore: elimina archivos obsoletos de Receta y guia de git que quedaron sueltos

--- src/main/java/com/elpaisa/dao/RecetaDao.java
+++
@@ -1,9 +0,0 @@
-package com.elpaisa.dao;
-
-import com.elpaisa.model.Receta;
-
-import java.util.List;
-
-public interface RecetaDao extends GenericDao<Receta, Integer> {
-    List<Receta> buscarPorProducto(Integer idProducto);
-}
